01ca3db6a490e6417c3552fa1e7e690477f908e8
John Hobbs <john@velvetcache.org>
2010-11-02 11:08:22 -0500

    Moved some things around to be orderly.

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ setup(
 	author_email='john@velvetcache.org',
 	url='http://github.com/jmhobbs/detours-clone',
 	py_modules=['detours'],
-	scripts=['detoursd','detours-gtk','detours-http'],
+	package_dir = { '': 'lib' },
+	scripts=['scripts/detoursd','scripts/detours-gtk','scripts/detours-http'],
 	data_files=[
-		( '/etc/init.d', ['init.d/detoursd'] ),
-		( '/etc', ['detoursd.conf'] )
+		( '/etc/init.d', ['assets/init.d/detoursd'] ),
+		( '/etc', ['assets/detoursd.conf'] )
 	]
 )
