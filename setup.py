f700bdce1d42d0f5b9fac39ec5b52eb1dbfa1b82
John Hobbs <john@velvetcache.org>
2010-11-03 23:19:23 -0500

    Enhanced the clients, use config files instead of hard coded port numbers.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from distutils.core import setup
 setup(
 	name='detours',
-	version='1.0',
+	version='1.0.1',
 	description='Easy Hosts File Editor',
 	author='John Hobbs',
 	author_email='john@velvetcache.org',
 	url='http://github.com/jmhobbs/detours-clone',
 	py_modules=['detours'],
 	package_dir = { '': 'lib' },
-	scripts=['scripts/detoursd','scripts/detours-gtk','scripts/detours-http'],
+	scripts=['scripts/detoursd','scripts/detours-gtk','scripts/detours-http', 'script/detours-qt'],
 	data_files=[
 		( '/etc/init.d', ['assets/init.d/detoursd'] ),
 		( '/etc', ['assets/detoursd.conf'] )
