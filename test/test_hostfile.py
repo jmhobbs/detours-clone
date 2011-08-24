dba7901f34c6054e3b38e66960f619b559362abc
John Hobbs <john@velvetcache.org>
2011-08-24 12:33:25 -0500

    Test for blank line error.

--- test/test_hostfile.py
+++ test/test_hostfile.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
-
-# Fix it so we import the dev version before anything else
-lib_path = os.path.abspath( os.path.join( os.path.dirname( os.path.abspath( __file__ ) ), '..', 'lib' ) )
-sys.path.insert( 0, lib_path )
-import hostfile
-del sys.path[0]
-
+import detours
 import os.path
 
 # Set the path to our data directory
@@ -15,23 +9,28 @@ DATA_PATH = "%s/data" % os.path.dirname( __file__ )
 class TestHostFile ( unittest.TestCase ):
 
 	def test_get_detours_exist ( self ):
-		hf = hostfile.HostFile( DATA_PATH + '/has-detours-section' )
+		hf = detours.HostFile( DATA_PATH + '/has-detours-section' )
 		self.assertEquals( hf.getDetours(), [ {'ip': '127.0.0.1', 'host': 'localhost'}, {'ip': '127.0.0.1', 'host': 'www.google.com'}] )
 
 	def test_get_detours_dont_exist ( self ):
-		hf = hostfile.HostFile( DATA_PATH + '/no-detours-section' )
+		hf = detours.HostFile( DATA_PATH + '/no-detours-section' )
 		self.assertEquals( hf.getDetours(), [] )
 
 	def test_get_detour_exist ( self ):
-		hf = hostfile.HostFile( DATA_PATH + '/has-detours-section' )
+		hf = detours.HostFile( DATA_PATH + '/has-detours-section' )
 		self.assertEquals( hf.findDetour( 'www.google.com' ), {'ip': '127.0.0.1', 'host': 'www.google.com'} )
 
 	def test_get_detour_doesnt_exist ( self ):
-		hf = hostfile.HostFile( DATA_PATH + '/has-detours-section' )
+		hf = detours.HostFile( DATA_PATH + '/has-detours-section' )
 		self.assertEquals( hf.findDetour( 'google.com' ), None )
 
-		hf = hostfile.HostFile( DATA_PATH + '/no-detours-section' )
+		hf = detours.HostFile( DATA_PATH + '/no-detours-section' )
 		self.assertEquals( hf.findDetour( 'google.com' ), None )
+
+	def test_get_detours_exist_with_blank_line ( self ):
+		hf = detours.HostFile( DATA_PATH + '/has-detours-section-with-blank-line' )
+		self.assertEquals( hf.getDetours(), [ {'ip': '127.0.0.1', 'host': 'localhost'}, {'ip': '127.0.0.1', 'host': 'www.google.com'}] )
 
 if __name__ == '__main__':
 	unitest.main()
+
